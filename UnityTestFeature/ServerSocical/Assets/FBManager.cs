45f73037745cc9fc38dc72cde5518a64f7cd6007
nguyenkim495 <nguyenkim495@gmail.com>
2017-05-12 00:27:05 +0700

    + Adding App42SDK

--- UnityTestFeature/ServerSocical/Assets/FBManager.cs
+++ UnityTestFeature/ServerSocical/Assets/FBManager.cs
@@ -8,10 +8,31 @@ using System;
 public class FBManager : MonoBehaviour
 {
     public Text userIdText;
-    
+    public string userID;
+    private static FBManager instance = null;
+    private static readonly object padlock = new object();
+
+    public static FBManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock(padlock)
+                {
+                    if(instance == null)
+                        instance = new FBManager();
+                }
+            }
+                
+            return instance;
+        }
+    }
+
     // Use this for initialization
     public void Awake()
     {
+        instance = new FBManager();
         if (!FB.IsInitialized)
         {
             FB.Init();
@@ -32,6 +53,8 @@ public class FBManager : MonoBehaviour
         {
             AccessToken token = AccessToken.CurrentAccessToken;
             userIdText.text = token.UserId;
+            userID = token.UserId;
+            Debug.Log("userID:" + userID);
         }
         else
             Debug.Log("Cacelled Login");
@@ -41,12 +64,16 @@ public class FBManager : MonoBehaviour
 
     public void Share()
     {
-        FB.ShareLink(contentTitle: "hello world",
-            contentDescription: "write something",
-            contentURL: new System.Uri("https://play.google.com/store"),
-            //photoURL: new System.Uri("link image"),
-            callback: ShareCallback
-            );
+        //FB.ShareLink(contentTitle: "hello world",
+        //    contentDescription: "write something",
+        //    contentURL: new System.Uri("https://play.google.com/store"),
+        //    //photoURL: new System.Uri("link image"),
+        //    callback: ShareCallback
+        //    );
+        FB.FeedShare(linkCaption: "hello world",
+            picture: new System.Uri("https://scontent-hkg3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/11002495_681185385326261_5595920565377356380_n.jpg?oh=709c1ebbeb3d74bec276d54ae611f222&oe=560E242F"),
+            linkName: "check out this game",
+            link: new System.Uri("https://www.facebook.com/"));
     }
 
     private void ShareCallback(IShareResult result)
@@ -67,7 +94,10 @@ public class FBManager : MonoBehaviour
 
     public void Invite()
     {
-
+        FB.Mobile.AppInvite(appLinkUrl: new System.Uri(""),
+            previewImageUrl: new System.Uri(""));
     }
+
+
 
 }
---
+++ UnityTestFeature/ServerSocical/Assets/UserManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.user;
+using com.shephertz.app42.paas.sdk.csharp.storage;
+using System;
+
+
+public class UserManager : MonoBehaviour {
+
+    private string apiKey = "cd18e9405709ff97ddfd5fb59b8d035ee75362e747f98e2e155c04a7423f26a7";
+    private string secretKey = "4523e8a6ed3604a6c2f508390e2929604d4ccdd1422f36053a783f5ad3b21a6b";
+    private string DataBaseName = "PANDATABASE";
+    ServiceAPI serviceAPI;
+    UserService userService;
+    List<string> listUserID;
+    public Text createUser;
+    public String jonsTest = "{\"userID\":\"1185743988204572\",\"rank\":\"chicken\",\"Highest Score\":\"19090\"}";
+
+    StorageService storageService = null;
+
+
+    // Use this for initialization
+    void Start () {
+       // fbManager = new FBManager();
+        App42Log.SetDebug(true);
+        serviceAPI = new ServiceAPI(apiKey, secretKey);
+        storageService = new StorageService(apiKey, secretKey);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateRank()
+    {
+        string userID;
+        storageService.InsertJSONDocument(DataBaseName, "NewCollection", jonsTest, callBack: new UpdateCallback());
+    }
+    public class UpdateCallback : App42CallBack
+    {
+        public void OnSuccess(object response)
+        {
+           
+
+        }
+        public void OnException(Exception e)
+        {
+            
+        }
+    }
+
+    public void CreatUser()
+    {
+        string userID = "1185743988204572"; //userID
+        Debug.Log("server" + userID);
+        userService = serviceAPI.BuildUserService();
+        userService.CreateUser(uName: userID, pwd: "NULL", emailAddress: "hung.nguyenvan2@gmail.com", callBack: new CreateUserCallback());
+    }
+    public class CreateUserCallback : App42CallBack
+    {
+        public void OnSuccess(object response)
+        {
+            User user = (User)response;
+            /* This will create user in App42 cloud and will return User object */
+            App42Log.Console("userName is " + user.GetUserName());
+            App42Log.Console("emailId is " + user.GetEmail());
+            
+        }
+        public void OnException(Exception e)
+        {
+            App42Log.Console("Exception : " + e);
+            App42Exception exception = (App42Exception)e;
+            int appErrorCode = exception.GetAppErrorCode();
+            if (appErrorCode == 2001)
+                Debug.Log("have early profile");
+        }
+    }
+
+    public void GetUserProfile(string userName)
+    {
+        userService = serviceAPI.BuildUserService();
+        userService.GetUser(userName: userName, callBack: GetUserProfileCallback);
+    }
+    public App42CallBack GetUserProfileCallback { get; private set; }
+    
+
+    public List<string> GetAllUserID()
+    {
+        userService = serviceAPI.BuildUserService();
+        userService.GetAllUsers(callBack: GetAllUsers);
+        
+        return listUserID;
+    }
+    public App42CallBack GetAllUsers { get; private set; }
+}
