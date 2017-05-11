d9c6b0e9cc24d4d8efc76904f056d71a6089392a
nguyenkim495 <nguyenkim495@gmail.com>
2017-05-11 11:03:30 +0700

    + Add Unity folder

---
+++ UnityTestFeature/ServerSocical/Assets/FBManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+using System;
+
+public class FBManager : MonoBehaviour
+{
+    public Text userIdText;
+    
+    // Use this for initialization
+    public void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init();
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+    public void Login()
+    {
+        FB.LogInWithReadPermissions(callback: OnLogIn);
+    }
+
+    private void OnLogIn(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            AccessToken token = AccessToken.CurrentAccessToken;
+            userIdText.text = token.UserId;
+        }
+        else
+            Debug.Log("Cacelled Login");
+    }
+
+    
+
+    public void Share()
+    {
+        FB.ShareLink(contentTitle: "hello world",
+            contentDescription: "write something",
+            contentURL: new System.Uri("https://play.google.com/store"),
+            //photoURL: new System.Uri("link image"),
+            callback: ShareCallback
+            );
+    }
+
+    private void ShareCallback(IShareResult result)
+    {
+        if(result.Cancelled || !string.IsNullOrEmpty(result.Error))
+        {
+            Debug.Log("share link error: " + result.Error);
+        }
+        else if (!String.IsNullOrEmpty(result.PostId))
+        {
+            Debug.Log(result.PostId);
+        }
+        else
+        {
+            Debug.Log("ShareLink success!");
+        }
+    }
+
+    public void Invite()
+    {
+
+    }
+
+}
